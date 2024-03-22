f5ee41af131796c6947933c3d91d01ba18b1dc79
edward0181 <edwardroggeveen@gmail.com>
2024-03-22 18:31:56 +0100

    Update twrp_a23.mk

--- twrp_a23.mk
+++ twrp_a23.mk
@@ -18,6 +18,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 # Inherit from our custom product configuration
 $(call inherit-product, vendor/twrp/config/common.mk)
 
+# Inherit device configuration
+$(call inherit-product, device/samsung/a23/device.mk)
+
 PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,device/samsung/a23/recovery/root,recovery/root)
 
 PRODUCT_DEVICE := a23
